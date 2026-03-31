7d45e3081cd2d38c805ce1739e5de7c3fd521f1d
DingFong <a890702000@gmail.com>
2026-03-31 01:32:15 -0400

    fix(streaming): replace deprecated torch_dtype with dtype

--- methods/streaming_llm.py
+++ methods/streaming_llm.py
@@ -92,7 +92,7 @@ class StreamingLLMMethod(BaseMethod):
         self.model = AutoModelForCausalLM.from_pretrained(
             model_name,
             device_map=device,
-            torch_dtype=torch.float16,
+            dtype=torch.float16,
         )
         self.model.eval()
 
